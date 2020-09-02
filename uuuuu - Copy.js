8976ca83e900c0ddce5dbf3d65ce9ed681346c3a
aakashdeveloper <ahanda205@gmail.com>
2020-09-02 19:49:32 +0530

    Q

---
+++ uuuuu - Copy.js
@@ -0,0 +1,24 @@
+DOM> Document Object Model
+
+id
+class
+tagname
+
+document.getElementsByTagName('h1')[0]
+<h1>​I'm AAKASH HANDA​</h1>​
+document.getElementsByTagName('h1')[0].innerText
+"I'm AAKASH HANDA"
+document.getElementsByTagName('h1')[0].innerText="I m form Edureka"
+"I m form Edureka"
+document.getElementsByTagName('h1')[0].style.color="Red"
+"Red"
+document.getElementsByTagName('h1')[0].style.visibility="hidden"
+"hidden"
+document.getElementsByTagName('h1')[0].style.visibility="visible"
+"visible"
+document.getElementsByClassName('scroll')
+HTMLCollection(5) [a.scroll, a.scroll, a.scroll, a.scroll, a.scroll]
+document.getElementsByClassName('scroll')[0]
+<a href=​"#skills" class=​"scroll">​Skills​</a>​
+document.getElementsByClassName('scroll')[0].innerText="Tech"
+"Tech"
